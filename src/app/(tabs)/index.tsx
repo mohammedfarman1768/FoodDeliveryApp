2d8d92ab5ed3524da3b18d3aa0665fe0a72ff478
Mohammed Farman <farmankhan1768@gmail.com>
2026-01-04 21:23:09 +0530

    props and first components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,43 +1,14 @@
-import { StyleSheet, Image, Text, View } from 'react-native';
-
+import { View } from 'react-native';
 import products from '../../../assets/data/products';
-import Colors from '../../constants/Colors';
-const product = products[0];
+import ProductListItem from '../../components/ProductListItem';
 
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image }}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+    <View>
+      <ProductListItem product={products[5]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 10,
-    overflow: 'hidden',
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-    alignSelf: 'center',
-  },
-  title: {
-    fontWeight: '600',
-    fontSize: 18,
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-    marginTop: 'auto',
-  },
-});
+
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Colors from '../constants/Colors';
+
+const ProductListItem = ({ product }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontWeight: '600',
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+    marginTop: 'auto',
+  },
+});
+export default ProductListItem;
